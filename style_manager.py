b6c65296254065fc4ecb89f229ab3723d969143c
Ahmad Nayfeh <ahmad2224466@gmail.com>
2025-07-14 10:14:44 +0300

    Update style_manager.py

--- style_manager.py
+++ style_manager.py
@@ -18,7 +18,7 @@ def apply_sidebar_styles():
             }
             
             /* --- Sidebar Link Font Size Increase --- */
-            [data-testid="stSidebarNav"] a {
+            body [data-testid="stSidebarNav"] a {
                 font-family: 'Inter', sans-serif;
                 font-size: 2.5em !important; /* زيادة كبيرة في حجم خط الشريط الجانبي */
                 padding: 14px 18px !important; /* تعديل الحشو ليتناسب مع الخط الكبير */
